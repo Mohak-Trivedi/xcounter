672767e683e277ab69a97932ffd50c69b00cc96c
Mohak-Trivedi <trivedimohak@gmail.com>
2023-12-13 18:17:38 +0530

    corrected import statement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import CounterApp from "./CounterApp";
+import CounterApp from "./Counter.jsx";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
